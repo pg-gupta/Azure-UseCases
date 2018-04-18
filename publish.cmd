10906ddbe78e45ffca5c042824515cf2d1029d01
Pooja Gupta <poojageetagupta@gmail.com>
2018-04-17 23:22:50 -0500

    uploading robodoc again

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=robodoc-Web-Deploy.pubxml -p:Password=zeDrhdgGhCMHF0Hp9GvifaoLAll3l93k4vfFElGpmRPzJivtYco9zMkSv1hR
+
